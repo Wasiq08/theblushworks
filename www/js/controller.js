d07ac3fd8edcdd10a82f1a533e11a24b45bfad5d
Wasiq08 <s.wasiq.muhammad@gmail.com>
2016-06-15 18:57:38 +0500

    post

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,15 +1,22 @@
 angular.module('starter')
 
-.controller('MenuCtrl',function ($http,$scope) {
+.controller('MenuCtrl',function ($http, $scope) {
     
     $scope.categories=[];
     $http.get("http://www.theblushworks.com/api/get_category_index/").then(function(returnData){
         $scope.categories = returnData.data.categories; 
         console.log(returnData);
     },function(err){
-        console.log(err);
+        console.log(err);  
+    })
+    
+    
+    $scope.recent_posts=[];
+    $http.get("http://www.theblushworks.com/api/get_posts/").then(function (data) {
         
-        
+        $scope.recent_posts = data.data.posts;
+    },function (err) {
+         console.log(err);  
     })
     
 })
