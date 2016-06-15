68f137fb9f8a08e9a1c5dfb28581a4950852be4c
Wasiq08 <s.wasiq.muhammad@gmail.com>
2016-06-15 21:44:07 +0500

    infinite scroll

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,8 +1,11 @@
 angular.module('starter')
 
-.controller('MenuCtrl',function ($http, $scope) {
+.controller('MenuCtrl',function ($http, $scope ,$sce ,$ionicScrollDelegate) {
     
     $scope.categories=[];
+    $scope.count_total = 1 ;
+    $scope.offset = 0 ; 
+    
     $http.get("http://www.theblushworks.com/api/get_category_index/").then(function(returnData){
         $scope.categories = returnData.data.categories; 
         console.log(returnData);
@@ -11,15 +14,114 @@ angular.module('starter')
     })
     
     
+    
+    
+    
+    
+   $scope.doRefresh = function(){
+    
     $scope.recent_posts=[];
     $http.get("http://www.theblushworks.com/api/get_posts/").then(function (data) {
         
         $scope.recent_posts = data.data.posts;
+        $scope.count_total = data.data.count_total; 
+        $scope.recent_posts.forEach(function(elem , index , array){
+         elem.excerpt = elem.excerpt.substr(0,100);
+         elem.excerpt = elem.excerpt + "...Read More";
+         elem.excerpt = $sce.trustAsHtml(elem.excerpt);
+        })
+            
+        
+        $scope.$broadcast('scroll.refreshComplete');
+        
+        
     },function (err) {
          console.log(err);  
-    })
+    })   
+       
+       
+   }
+   
+   
+   
+    
+    $scope.recent_posts=[];
+    $http.get("http://www.theblushworks.com/api/get_posts/").then(function (data) {
+        
+        $scope.recent_posts = data.data.posts;
+        $scope.count_total = data.data.count_total; 
+        $scope.recent_posts.forEach(function(elem , index , array){
+         elem.excerpt = elem.excerpt.substr(0,100);
+         elem.excerpt = elem.excerpt + "...Read More";
+         elem.excerpt = $sce.trustAsHtml(elem.excerpt);
+        })
+            
+        
+        $scope.$broadcast('scroll.refreshComplete');
+        
+        
+    },function (err) {
+         console.log(err);  
+    })   
+   
+   
+   
+   
+   
+   
+   
+   
+    
+    $scope.canLoadMore = function(){
+        return true;
+    }
+    
+    $scope.timer =  new Date().getTime();
+    $scope.lastTimer = new Date().getTime();
+    
+    $scope.loadMore = function(){
+        
+          $scope.timer =  new Date().getTime();
+            
+          if(new Date ($scope.timer - $scope.lastTimer) > 5000 ) 
+           {
+              
+              $scope.lastTimer = new Date().getTime();
+              
+              
+          $http.get("http://www.theblushworks.com/api/get_posts/?offset="+$scope.offset).then(function (data) {
+            var newPosts = data.data.posts;
+            $scope.count_total = data.data.count_total; 
+         
+                  
+                    newPosts.forEach(function(elem , index , array){
+                    elem.excerpt = elem.excerpt.substr(0,100);
+                    elem.excerpt = elem.excerpt + "...Read More";
+                    elem.excerpt = $sce.trustAsHtml(elem.excerpt);
+                    })
+                                
+                  $scope.recent_posts.push.apply($scope.recent_posts , newPosts);
+                  $scope.$broadcast('scroll.infiniteScrollComplete');     
+                  $scope.offset +=10;  
+    
+        });
+              
+                       
+    }
+        
+    }
+    
+    
+    $scope.searchTextChanged = function(){
+    $ionicScrollDelegate.$getByHandle('mainScroll').scrollTop(true);
+    }
+
+    
+    
     
 })
+
+
 
 
 .controller('PostCtrl',function () {
